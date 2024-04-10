b294d6a8f8105cf820fbe79147571a3bbbf8897a
Rescor <rescorent@gmail.com>
2024-04-10 16:26:18 +0200

    Add the ENV variables support

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const path = require('path');
 
 AWS.config.update({
-  accessKeyId: '',
-  secretAccessKey: '',
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID || '',
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || '',
 });
 
 const s3 = new AWS.S3();
