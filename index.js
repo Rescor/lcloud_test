7cce91ec46cd2e7b3803675174c9e0f4197277f1
Rescor <rescorent@gmail.com>
2024-04-10 15:19:11 +0200

    Add upload local files function

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
 
 AWS.config.update({
   accessKeyId: '',
@@ -6,8 +8,9 @@ AWS.config.update({
 });
 
 const s3 = new AWS.S3();
+const localFilesFolder = 'files';
 
-// Get files in the bucket
+// List files in the bucket
 async function listAllFiles(bucket, prefix) {
   const params = {
     Bucket: bucket,
@@ -18,4 +21,24 @@ async function listAllFiles(bucket, prefix) {
   console.log("Files in bucket:", data.Contents.map(object => object.Key));
 }
 
+// Upload files to the bucket
+async function uploadLocalFiles(bucket, folder) {
+  const files = fs.readdirSync(folder);
+
+  for (const file of files) {
+    const filePath = path.join(folder, file);
+    const fileContent = fs.readFileSync(filePath);
+
+    const params = {
+      Bucket: bucket,
+      Key: `a-wing/${file}`,
+      Body: fileContent
+    };
+
+    await s3.upload(params).promise();
+    console.log(`File ${file} uploaded successfully.`);
+  }
+}
+
 listAllFiles('developer-task', 'a-wing');
+uploadLocalFiles('developer-task', localFilesFolder);
